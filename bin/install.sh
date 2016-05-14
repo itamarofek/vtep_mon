ede157a845a0d2eb1a3d74317eec29beb54c9928
itamar <itamar.ofek@huawei.com>
2016-05-14 07:28:36 +0100

    wip need to fix

--- bin/install.sh
+++ bin/install.sh
@@ -2,20 +2,37 @@
 
 # based on ubuntu 14.04.03
 
+exec_dirs
 
 
-
+OVS_REP0=$HOME/ovs
 FROM_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 PYTHON_PKG_DIR=/usr/lib/python2.7/dist-packages
 
-bin_files='vtep-mon'
+sudo python setup.py install --record /tmp/files.txt
+cat /tmpfiles.txt |xargs sudo rm -rf
+sudo python setup.py install
+
+
+sudo rm -f /etc/init/vtep-mon
+sudo cp -r $FROM_DIR/etc/init/* /etc/init
+
+rm -rf /etc/vtep_mon
+sudo cp -r $FROM_DIR/etc/vtep_mon /etc
+
+cat $FROM_DIR/etc/vtep_mon/rootwrap.conf | awk '/^exec_dirs/  {$0=$0",$HOME/ovs/vtep"} ; {print}' > /etc/vtep_mon/rootwrap.conf 
+
+
+
+
+
 
 mkdir /var/log/vtep_mon
 
 
 # clean
-apt-get clean
-rm -f /var/lib/apt/lists/*
-cat /dev/zero > zero
-rm -f zero
+sudo apt-get clean
+sudo rm -f /var/lib/apt/lists/*
+sudo cat /dev/zero > zero
+sudo rm -f zero
 
